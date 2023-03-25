ef982a9b68bcc3ebe8668f9fe2c365dde8fc5769
Vladimir Kotov <tovkotov@yandex.ru>
2023-03-25 09:23:23 +0600

    заработала подпись документов

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const contractAddress = "0x771081962f7b173eAdf523945c5994e53d0Ab304";
 const abi = [
   {
     anonymous: false,
@@ -17,9 +16,9 @@ const abi = [
       },
       {
         indexed: false,
-        internalType: "bytes32",
+        internalType: "bytes",
         name: "signatureHash",
-        type: "bytes32",
+        type: "bytes",
       },
     ],
     name: "DocumentSigned",
@@ -33,9 +32,9 @@ const abi = [
         type: "bytes32",
       },
       {
-        internalType: "bytes32",
+        internalType: "bytes",
         name: "signatureHash",
-        type: "bytes32",
+        type: "bytes",
       },
     ],
     name: "signDocument",
@@ -51,9 +50,9 @@ const abi = [
         type: "bytes32",
       },
       {
-        internalType: "bytes32",
+        internalType: "bytes",
         name: "signatureHash",
-        type: "bytes32",
+        type: "bytes",
       },
       {
         internalType: "address",
@@ -73,84 +72,145 @@ const abi = [
     type: "function",
   },
 ];
+const contractAddress = "0xa45A1689d97767CB6c700D67bCf7d94352834BBD";
 
-const provider = new ethers.providers.Web3Provider(window.ethereum, 97);
-
-let signer;
+let documentHash;
+let signers = new Set();
+let web3;
 let contract;
+let userAccount;
 
-const fileInput = document.getElementById("file-input");
-const hashOutput = document.getElementById("hash-output");
-const signerInput = document.getElementById("signer-input");
-const signerList = document.getElementById("signer-list");
-const nameInput = document.getElementById("name-input");
-const addSignerButton = document.getElementById("add-signer-button");
-const signButton = document.getElementById("sign-button");
-const checkSignaturesButton = document.getElementById(
-  "check-signatures-button"
-);
-
-const signers = new Set();
-
-fileInput.addEventListener("change", async () => {
-  const file = fileInput.files[0];
-  const hash = await calculateSHA256(file);
-  hashOutput.value = hash;
-});
-
-provider.addEventListener("eth_requestAccounts", []).then(() => {
-  // provider.listAccounts().then((accounts) => {
-  //   signer = provider.getSigner(accounts[0]);
-  //   contract = new ethers.Contract(contractAddress, abi, signer);
-  // });
-  const signer = signerInput.value.trim();
-  if (signer) {
-    signers.add(signer);
-    signerList.value = [...signers].join("\n");
-    signerInput.value = "";
-  }
-});
-
-addSignerButton.addEventListener("click", () => {
-  const signer = signerInput.value.trim();
-  if (signer) {
-    signers.add(signer);
-    signerList.value = [...signers].join("\n");
-    signerInput.value = "";
-  }
-});
-
-signButton.addEventListener("click", () => {
-  const name = nameInput.value.trim();
-  if (name && signers.has(name)) {
-    alert("Document signed");
-  }
-});
-
-checkSignaturesButton.addEventListener("click", () => {
-  if (signers.size > 0) {
-    alert([...signers].join(", "));
+async function init() {
+  if (window.ethereum) {
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      web3 = new Web3(window.ethereum);
+      contract = new web3.eth.Contract(abi, contractAddress);
+      userAccount = (await web3.eth.getAccounts())[0];
+    } catch (error) {
+      console.error(error);
+    }
   } else {
-    alert("none");
+    alert("Please install MetaMask!");
   }
-});
+}
 
-function calculateSHA256(file) {
+async function getDocumentHash(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onload = () => {
-      const buffer = reader.result;
-      crypto.subtle
-        .digest("SHA-256", buffer)
-        .then((hash) => {
-          const hexString = Array.from(new Uint8Array(hash))
-            .map((b) => b.toString(16).padStart(2, "0"))
-            .join("");
-          resolve(hexString);
-        })
-        .catch(reject);
+    reader.onload = async function (event) {
+      const arrayBuffer = event.target.result;
+      const wordArray = CryptoJS.lib.WordArray.create(arrayBuffer);
+      const hash = CryptoJS.SHA3(wordArray, { outputLength: 256 }).toString();
+      resolve(hash);
     };
     reader.onerror = reject;
     reader.readAsArrayBuffer(file);
   });
 }
+
+document
+  .getElementById("uploadDocument")
+  .addEventListener("change", async (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      documentHash = await getDocumentHash(file);
+      document.getElementById("documentHash").innerText = documentHash;
+    }
+  });
+
+document.getElementById("addSigner").addEventListener("click", () => {
+  const signerAddress = document.getElementById("signerAddress").value.trim();
+  if (web3.utils.isAddress(signerAddress)) {
+    signers.add(signerAddress);
+    document.getElementById("signerList").value =
+      Array.from(signers).join("\n");
+  } else {
+    alert("Invalid address");
+  }
+});
+
+// document.getElementById("signDocument").addEventListener("click", async () => {
+//   const signerAddress = document.getElementById("signerAddress").value;
+//   const documentHash = document.getElementById("documentHash").innerText;
+//   const paddedDocumentHash = web3.utils.padLeft("0x" + documentHash, 64);
+//   const documentHashBytes = web3.utils.hexToBytes(paddedDocumentHash);
+
+//   if (web3.utils.isAddress(signerAddress)) {
+//     try {
+//       // Sign the document hash
+//       const signature = await web3.eth.sign(paddedDocumentHash, signerAddress);
+//       const signatureBytes = "0x" + signature.slice(2);
+
+//       // Send the transaction
+//       const tx = await contract.methods
+//         .signDocument(documentHashBytes, signatureBytes)
+//         .send({ from: signerAddress });
+
+//       document.getElementById("signStatus").innerText = "Document signed";
+//     } catch (err) {
+//       console.error(err);
+//       document.getElementById("signStatus").innerText =
+//         "Error signing the document";
+//     }
+//   } else {
+//     document.getElementById("signStatus").innerText = "Invalid signer address";
+//   }
+// });
+
+document.getElementById("signDocument").addEventListener("click", async () => {
+  const signerAddress = document.getElementById("signerAddress").value;
+  const documentHash = document.getElementById("documentHash").innerText;
+  const paddedDocumentHash = web3.utils.padLeft("0x" + documentHash, 64);
+
+  if (web3.utils.isAddress(signerAddress)) {
+    try {
+      // Sign the document hash
+      const signature = await web3.eth.sign(paddedDocumentHash, signerAddress);
+
+      // Encode the parameters for the signDocument function
+      const encodedParameters = web3.eth.abi.encodeParameters(
+        ["bytes32", "bytes"],
+        [paddedDocumentHash, signature]
+      );
+
+      // Send the transaction
+      const tx = await contract.methods
+        .signDocument(paddedDocumentHash, encodedParameters)
+        .send({ from: signerAddress });
+
+      document.getElementById("signStatus").innerText = "Document signed";
+    } catch (err) {
+      console.error(err);
+      document.getElementById("signStatus").innerText =
+        "Error signing the document";
+    }
+  } else {
+    document.getElementById("signStatus").innerText = "Invalid signer address";
+  }
+});
+
+document
+  .getElementById("checkSignatures")
+  .addEventListener("click", async () => {
+    const signedAddresses = [];
+
+    for (const signer of signers) {
+      const signatureHash = web3.utils.sha3(documentHash + signer);
+      const isSigned = await contract.methods
+        .verifySignature(documentHash, signatureHash, signer)
+        .call();
+      if (isSigned) {
+        signedAddresses.push(signer);
+      }
+    }
+
+    if (signedAddresses.length > 0) {
+      document.getElementById("signatureList").value =
+        signedAddresses.join("\n");
+    } else {
+      document.getElementById("signatureList").value = "No signatures found";
+    }
+  });
+
+init();
